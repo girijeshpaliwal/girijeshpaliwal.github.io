5ebc46391209536623e3ff9802604046d3f41144
paliwalgirijesh-ops <paliwalgirijesh@gmail.com>
2026-08-17 13:33:59 +0530

    Add immediate redirect to external URL
    Redirect users to the specified URL before rendering the app.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Perform the redirect immediately
+window.location.replace('https://girijeshpaliwal.vercel.app/');
+
+// You can safely remove the createRoot(...) block below 
+// since the user will be redirected before the app renders.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
