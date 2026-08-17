439bdd73d05036470e246ac5f5318bd9883687b6
paliwalgirijesh-ops <paliwalgirijesh@gmail.com>
2026-08-17 12:57:03 +0530

    Fix import path for ParallaxBackground component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
+import ParallaxBackground from "../components/parallaxBackground";
 import { Astronaut } from "../components/Astronaut";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
@@ -10,10 +10,13 @@ import Loader from "../components/Loader";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
+
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
+
       <ParallaxBackground />
+
       <figure
         className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
@@ -26,6 +29,7 @@ const Hero = () => {
                 position={isMobile && [0, -1.5, 0]}
               />
             </Float>
+
             <Rig />
           </Suspense>
         </Canvas>
